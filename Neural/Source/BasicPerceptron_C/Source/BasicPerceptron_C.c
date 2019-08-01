87733b553f17948aaf937f07556343f33fe4b3f0
Robot-Fromage <code@clementberthaud.com>
2019-08-01 19:07:42 +0200

    infeat: imp C project

---
+++ Neural/Source/BasicPerceptron_C/Source/BasicPerceptron_C.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+*
+*   AdAD-Neural
+*__________________
+*
+* BasicPerceptron.cpp
+* Layl
+* Please refer to LICENSE.md
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+int main()
+{
+    printf( "%i", 1 );
+    return  0;
+}
